b3e48acdfeb6558fe315dbb8bad1a9191e26ccff
umer1317 <umerfarrukh1317@gmail.com>
2024-04-01 20:53:18 +0000

    adding my learning :sparkles:

--- lesson3/HelloBlazePreprocessLambda.py
+++ lesson3/HelloBlazePreprocessLambda.py
@@ -5,8 +5,8 @@ import os
 import boto3
 from botocore.exceptions import ClientError
 
-BUCKET_NAME = 'FILL_THIS_IN'
-PREFIX = 'FILL_THIS_IN'
+BUCKET_NAME = 'sagemaker-us-east-1-474399255424'
+PREFIX = 'farrukh'
 
 # Function below unzips the archive to the local directory. 
 
